a2e6e50cd26e72a0b41ea47f0f3fbf2df79b72e5
Cas de Rooij <casdrooij@gmail.com>
2018-03-23 10:16:29 +0100

    Add dotEnv library and setup of .env file

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports = {
+    port: process.env.SERVER_POORT || 3000,
+    dbConnection: {
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'db',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: process.env.DB_DIALECT || 'mysql',
+        operatorsAliases: false
+    },
+    saltRounds: process.env.SALT_ROUNDS,
+    jwtSecret: process.env.JWT_SECRET || 'secret_key',
+    tokenExpireTime: process.env.TOKEN_EXPIRE_TIME || '6h'
+}
