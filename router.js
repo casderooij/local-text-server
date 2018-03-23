ee1c4a8863c2216975ed236bde91572435de5ff8
Cas de Rooij <casdrooij@gmail.com>
2018-03-23 09:49:27 +0100

    Initial commit

---
+++ router.js
@@ -0,0 +1,13 @@
+const authController = require('./controllers/auth');
+const orderController = require('./controllers/order');
+const authMiddleware = require('./middlewares/auth');
+
+module.exports.set = app => {
+    app.post('/login', authController.login);
+    app.post('/register', authController.register);
+
+    app.get('/orders', authMiddleware.checkAuth, orderController.getOrders);
+    app.get('/orders/:id', authMiddleware.checkAuth, orderController.getOrder);
+    app.post('/orders', authMiddleware.checkAuth, orderController.addOrder);
+    // app.get('/user_orders', authMiddleware.checkAuth, userController.getUsersWithOrders);
+}
