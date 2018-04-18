8b527f20bbc34a0e55268c420e14682cb1cb1f6e
Cas de Rooij <casdrooij@gmail.com>
2018-04-18 14:38:52 +0200

    CORS

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('client'));
 // Middleware for allowing Vuejs App to communicate with the server
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, token");
     next();
 });
